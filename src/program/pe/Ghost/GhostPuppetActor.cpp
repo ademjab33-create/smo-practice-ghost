bd37f7b7a6779a704b0ee32f78376cde2ccb8c80
ademjab33-create <adem.jab33@gmail.com>
2026-09-22 19:43:49 +0200

    feat: GhostPuppetActor.cpp

---
+++ src/program/pe/Ghost/GhostPuppetActor.cpp
@@ -0,0 +1,37 @@
+#include "pe/Ghost/GhostPuppetActor.h"
+#include "al/Library/LiveActor/ActorInitFunction.h"
+#include "al/Library/LiveActor/ActorFlagFunction.h"
+#include "al/Library/LiveActor/ActorClippingFunction.h"
+#include "al/Library/LiveActor/ActorModelFunction.h"
+#include "al/Library/LiveActor/ActorSensorFunction.h"
+#include "al/Library/LiveActor/ActorPoseKeeper.h"
+
+namespace pe {
+
+GhostPuppetActor::GhostPuppetActor(const char* name) : al::LiveActor(name) {
+    mGhostAlpha = 0.45f;
+}
+
+void GhostPuppetActor::init(const al::ActorInitInfo& info) {
+    al::initActorWithArchiveName(this, info, sead::SafeString("PlayerActorHakoniwa"), nullptr);
+    al::invalidateHitSensors(this);
+    al::invalidateClipping(this);
+    al::hideSilhouetteModelIfShow(this);
+    al::setModelAlphaMask(this, mGhostAlpha);
+    makeActorDead();
+}
+
+void GhostPuppetActor::control() {}
+
+void GhostPuppetActor::applyPosition(const sead::Vector3f& pos) {
+    al::setTrans(this, pos);
+}
+
+void GhostPuppetActor::setGhostAlpha(float alpha) {
+    if (alpha < 0.05f) alpha = 0.05f;
+    if (alpha > 1.0f) alpha = 1.0f;
+    mGhostAlpha = alpha;
+    al::setModelAlphaMask(this, mGhostAlpha);
+}
+
+} // namespace pe

24ebd98dd885b5a8c859015baedf887d397de603
ademjab33-create <adem.jab33@gmail.com>
2026-09-22 19:25:21 +0200

    feat: main.cpp with PBOverlay + timer combos

--- src/program/main.cpp
+++ src/program/main.cpp
@@ -1,5 +1,6 @@
 #include "al/Library/Memory/MemorySystem.h"
 #include "al/Library/Sequence/Sequence.h"
+#include "al/Library/Controller/JoyPadUtil.h"
 #include "diag/assert.hpp"
 #include "heap/seadExpHeap.h"
 #include "heap/seadHeapMgr.h"
@@ -12,74 +13,66 @@
 #include "pe/Menu/UserConfig.h"
 #include "pe/Util/Offsets.h"
 #include "program/imgui_nvn.h"
+#include "pe/Ghost/GhostManager.h"
 #include "pe/Ghost/ILTracker.h"
 #include "pe/Ghost/PBStorage.h"
+#include "pe/Ghost/PBOverlay.h"
 #include <sead/filedevice/seadFileDeviceMgr.h>
 
 HOOK_DEFINE_TRAMPOLINE(FileDeviceMgrCtor) { static void Callback(sead::FileDeviceMgr * thisPtr); };
-void FileDeviceMgrCtor::Callback(sead::FileDeviceMgr* thisPtr)
-{
-    Orig(thisPtr);
-    thisPtr->mMountedSd = nn::fs::MountSdCardForDebug("sd").IsSuccess();
-}
+void FileDeviceMgrCtor::Callback(sead::FileDeviceMgr* thisPtr) { Orig(thisPtr); thisPtr->mMountedSd = nn::fs::MountSdCardForDebug("sd").IsSuccess(); }
 
 class HakoniwaSequence;
 HOOK_DEFINE_TRAMPOLINE(HakoniwaSequenceInit) { static void Callback(HakoniwaSequence * thisPtr, const al::SequenceInitInfo& info); };
-void HakoniwaSequenceInit::Callback(HakoniwaSequence* thisPtr, const al::SequenceInitInfo& info)
-{
+void HakoniwaSequenceInit::Callback(HakoniwaSequence* thisPtr, const al::SequenceInitInfo& info) {
     Orig(thisPtr, info);
-
     pe::getMenuHeap() = sead::ExpHeap::create(1024 * 1024 * 4, "MenuHeap", al::getSequenceHeap(), 8, sead::ExpHeap::cHeapDirection_Forward, false);
-
     sead::ScopedCurrentHeapSetter setter(pe::getMenuHeap());
-    pe::PBStorage::createInstance(nullptr);
-    pe::PBStorage::instance()->init();
-    pe::ILTracker::createInstance(nullptr);
-    pe::ILTracker::instance()->init();
+    pe::PBStorage::createInstance(nullptr);  pe::PBStorage::instance()->init();
+    pe::ILTracker::createInstance(nullptr);  pe::ILTracker::instance()->init();
+    pe::GhostManager::createInstance(nullptr);
     pe::Menu::createInstance(nullptr);
 }
 
 HOOK_DEFINE_TRAMPOLINE(HakoniwaSequenceUpdate) { static void Callback(HakoniwaSequence * thisPtr); };
-void HakoniwaSequenceUpdate::Callback(HakoniwaSequence* thisPtr)
-{
+void HakoniwaSequenceUpdate::Callback(HakoniwaSequence* thisPtr) {
     Orig(thisPtr);
-
     sead::ScopedCurrentHeapSetter setter(pe::getMenuHeap());
     auto* menu = pe::Menu::instance();
     if (menu) {
         al::Scene* scene = *reinterpret_cast<al::Scene**>(reinterpret_cast<u8*>(thisPtr) + 0xb0);
         menu->update(scene);
     }
+    auto* ghostMgr = pe::GhostManager::instance();
+    if (ghostMgr) {
+        ghostMgr->update();
+        u32 trig = al::getPadTrigger(-1);
+        u32 held = al::getPadHold(-1);
+        bool rHeld = (held & 0x80) != 0;
+        if (rHeld && (trig & 0x2000)) ghostMgr->startRun();
+        if (rHeld && (trig & 0x8000)) ghostMgr->stopRun();
+        if (rHeld && (trig & 0x1000)) ghostMgr->resetRun();
+    }
 }
 
-static void drawDbgGui()
-{
+static void drawDbgGui() {
     sead::ScopedCurrentHeapSetter setter(pe::getMenuHeap());
     auto* menu = pe::Menu::instance();
-    if (menu)
-        menu->draw();
+    if (menu) menu->draw();
+    pe::PBOverlay::draw();
 }
 
-extern "C" void exl_main(void* x0, void* x1)
-{
+extern "C" void exl_main(void* x0, void* x1) {
     exl::hook::Initialize();
-
     using Patcher = exl::patch::CodePatcher;
     using namespace exl::patch::inst;
-
     FileDeviceMgrCtor::InstallAtOffset(pe::offsets::FileDeviceMgrCtorHookLocation);
     HakoniwaSequenceInit::InstallAtOffset(pe::offsets::HakoniwaSequenceInitHookLocation);
     HakoniwaSequenceUpdate::InstallAtOffset(pe::offsets::HakoniwaSequenceUpdate);
-
     pe::initMenuDPadDisableHooks();
     pe::installPracticeHacks();
-
     nvnImGui::InstallHooks();
     nvnImGui::addDrawFunc(drawDbgGui);
 }
 
-extern "C" NORETURN void exl_exception_entry()
-{
-    /* TODO: exception handling */
-    EXL_ABORT(0x420);
-}
+extern "C" NORETURN void exl_exception_entry() { EXL_ABORT(0x420); }

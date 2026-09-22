0fb6bde5627269ba4e85acfd7e3ed083e8af6ac3
ademjab33-create <adem.jab33@gmail.com>
2026-09-22 19:43:52 +0200

    feat: GhostManager with puppet + recording

--- include/pe/Ghost/GhostManager.h
+++ include/pe/Ghost/GhostManager.h
@@ -2,9 +2,11 @@
 #include "pe/Ghost/GhostTypes.h"
 #include <heap/seadDisposer.h>
 
-namespace al { class Scene; }
+namespace al { class Scene; class ActorInitInfo; }
 
 namespace pe {
+
+class GhostPuppetActor;
 
 class GhostManager {
     SEAD_SINGLETON_DISPOSER(GhostManager);
@@ -12,10 +14,11 @@ class GhostManager {
     ~GhostManager();
 
 public:
+    void initPuppet(const al::ActorInitInfo& info);
     void startRun();
     void stopRun();
     void resetRun();
-    void update();
+    void update(al::Scene* scene);
 
     bool isRunActive() const { return mIsRunActive; }
     s64 getElapsedTicks() const;
@@ -24,11 +27,23 @@ public:
     void clearNewPB() { mIsNewPB = false; }
 
 private:
+    void recordFrame(al::Scene* scene);
+    void playbackFrame();
+    void saveGhostData();
+    void loadGhostData();
+
     bool mIsRunActive = false;
     s64 mStartTick = 0;
     s64 mEndTick = 0;
     u32 mElapsedFrames = 0;
     bool mIsNewPB = false;
+
+    GhostPuppetActor* mPuppet = nullptr;
+    GhostFrame* mRecordBuf = nullptr;
+    GhostFrame* mPlayBuf = nullptr;
+    int mRecordCount = 0;
+    int mPlayCount = 0;
+    int mPlayStep = 0;
 };
 
 } // namespace pe

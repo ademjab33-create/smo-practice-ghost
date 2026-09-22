ea07d5c27bc7dcfbadb81ed2e516fea115445097
ademjab33-create <adem.jab33@gmail.com>
2026-09-22 19:02:11 +0200

    feat: add GhostTypes.h

---
+++ include/pe/Ghost/GhostTypes.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <types.h>
+
+namespace pe {
+
+enum class KingdomId : u8 {
+    Unknown = 0, Cap, Cascade, Sand, Lake, Wooded, Cloud, Lost,
+    Metro, Snow, Seaside, Luncheon, Ruined, Bowser, Moon, DarkSide, DarkerSide, Mushroom
+};
+
+enum class SegmentId : u8 {
+    FullKingdom = 0, BowserBunnies, BowserOdyssey, MoonEnd, CapEnd,
+    CascadeOdyssey, SandOdyssey, LakeOdyssey, WoodedOdyssey, CloudSkip,
+    LostOdyssey, MetroOdyssey, SnowOdyssey, SeasideOdyssey, LuncheonOdyssey, RuinedOdyssey
+};
+
+struct ReplayFrame {
+    u32 mStep = 0;
+    float mPlayerX = 0, mPlayerY = 0, mPlayerZ = 0;
+    u32 mButtons = 0;
+};
+
+struct GhostHeader {
+    u32 mMagic = 0x47485354; // 'GHST'
+    u32 mVersion = 1;
+    KingdomId mKingdomId = KingdomId::Unknown;
+    SegmentId mSegmentId = SegmentId::FullKingdom;
+    char mSegmentName[32] = {};
+    s64 mTotalTicks = 0;
+    u32 mTotalFrames = 0;
+};
+
+struct PBRecord {
+    KingdomId mKingdomId = KingdomId::Unknown;
+    SegmentId mSegmentId = SegmentId::FullKingdom;
+    char mSegmentName[32] = {};
+    s64 mBestTicks = 0;
+    u32 mBestFrames = 0;
+};
+
+} // namespace pe

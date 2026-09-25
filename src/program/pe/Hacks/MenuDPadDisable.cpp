350e4fd2c2d12b9593a8b010ff538d79259374d4
ademjab33-create <adem.jab33@gmail.com>
2026-09-25 23:47:30 +0200

    sync: src/program/pe/Hacks/MenuDPadDisable.cpp

--- src/program/pe/Hacks/MenuDPadDisable.cpp
+++ src/program/pe/Hacks/MenuDPadDisable.cpp
@@ -13,48 +13,36 @@ HOOK_DEFINE_TRAMPOLINE(DPadDown) { static bool Callback(int port); };
 HOOK_DEFINE_TRAMPOLINE(DPadLeft) { static bool Callback(int port); };
 HOOK_DEFINE_TRAMPOLINE(DPadRight) { static bool Callback(int port); };
 
+static bool dpadHook(int port, int mask)
+{
+    if (pe::Menu::instance()->isEnabled())
+        return false;
+    return al::isPadTrigger(port, mask);
+}
+
 bool DPadUp::Callback(int port)
 {
-    auto* menu = pe::Menu::instance();
-    if (menu && menu->isEnabled())
+    if (getConfig()->mDUpBind != ActionType::None)
         return false;
-    auto* cfg = getConfig();
-    if (cfg && cfg->mDUpBind != ActionType::None)
-        return false;
-    return Orig(port);
+    return dpadHook(port, 1 << 16);
 }
-
 bool DPadDown::Callback(int port)
 {
-    auto* menu = pe::Menu::instance();
-    if (menu && menu->isEnabled())
+    if (getConfig()->mDDownBind != ActionType::None)
         return false;
-    auto* cfg = getConfig();
-    if (cfg && cfg->mDDownBind != ActionType::None)
-        return false;
-    return Orig(port);
+    return dpadHook(port, 1 << 17);
 }
-
 bool DPadLeft::Callback(int port)
 {
-    auto* menu = pe::Menu::instance();
-    if (menu && menu->isEnabled())
+    if (getConfig()->mDLeftBind != ActionType::None)
         return false;
-    auto* cfg = getConfig();
-    if (cfg && cfg->mDLeftBind != ActionType::None)
-        return false;
-    return Orig(port);
+    return dpadHook(port, 1 << 18);
 }
-
 bool DPadRight::Callback(int port)
 {
-    auto* menu = pe::Menu::instance();
-    if (menu && menu->isEnabled())
+    if (getConfig()->mDRightBind != ActionType::None)
         return false;
-    auto* cfg = getConfig();
-    if (cfg && cfg->mDRightBind != ActionType::None)
-        return false;
-    return Orig(port);
+    return dpadHook(port, 1 << 19);
 }
 
 void initMenuDPadDisableHooks()

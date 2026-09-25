98aea97ffad580935333140b90459f1464edf9a4
ademjab33-create <adem.jab33@gmail.com>
2026-09-25 23:35:53 +0200

    feat: add Practice 1.0 options and Auto Kingdom Timer (src/program/pe/Hacks/PracticeHacks.cpp)

--- src/program/pe/Hacks/PracticeHacks.cpp
+++ src/program/pe/Hacks/PracticeHacks.cpp
@@ -8,14 +8,18 @@
 #include "al/Library/LiveActor/LiveActor.h"
 #include "al/Library/Math/MathRandomUtil.h"
 #include "al/Library/Nerve/NerveUtil.h"
+#include "al/Library/LiveActor/ActorPoseUtil.h"
 #include "hook/trampoline.hpp"
 #include "patch/code_patcher.hpp"
 #include "pe/Menu/Menu.h"
 #include "pe/Menu/UserConfig.h"
 #include "pe/Util/Offsets.h"
+#include "pe/Ghost/ILTracker.h"
+#include "Player/PlayerActorHakoniwa.h"
 #include "replace.hpp"
 #include "util/modules.hpp"
 #include "util/sys/rw_pages.hpp"
+#include <cmath>
 
 namespace pe {
 
@@ -23,6 +27,36 @@ HOOK_DEFINE_TRAMPOLINE(IsGotShine) { static bool Callback(void* accessor, void* 
 HOOK_DEFINE_TRAMPOLINE(SetGotShine) { static void Callback(GameDataHolderWriter writer, void* shineInfo); };
 HOOK_DEFINE_TRAMPOLINE(StartBgm1) { static void Callback(al::IUseAudioKeeper * user, const char* bgm, int a, int b); };
 HOOK_DEFINE_TRAMPOLINE(StartBgm2) { static void Callback(al::IUseAudioKeeper * user, void* request, bool a, bool b); };
+HOOK_DEFINE_TRAMPOLINE(IsPadTriggerA) { static bool Callback(s32 port); };
+HOOK_DEFINE_TRAMPOLINE(DoCheckpointTouchNotify) { static void Callback(al::LiveActor * checkpoint); };
+
+// Hooks from Practice 1.0 (BTT-Studio)
+HOOK_DEFINE_TRAMPOLINE(NoDamageHook) { static void Callback(GameDataHolderWriter writer); };
+HOOK_DEFINE_TRAMPOLINE(WarpTextHook) { static bool Callback(void* accessor); };
+HOOK_DEFINE_TRAMPOLINE(RefreshPurpsHook) { static bool Callback(void* accessor, const void* info); };
+HOOK_DEFINE_TRAMPOLINE(DoorRefreshHook) { static void Callback(al::LiveActor* door, const void* info); };
+HOOK_DEFINE_TRAMPOLINE(SeedGrowTimeHook) { static int Callback(const al::LiveActor* actor, const void* id); };
+HOOK_DEFINE_TRAMPOLINE(SeedUsedHook) { static bool Callback(const al::LiveActor* actor, const void* id); };
+HOOK_DEFINE_TRAMPOLINE(KingdomEnterHook) { static bool Callback(void* data, int i); };
+HOOK_DEFINE_TRAMPOLINE(DisableMoonLockHook) { static int Callback(GameDataHolder* thisPtr, bool* isCrashList, int worldID); };
+HOOK_DEFINE_TRAMPOLINE(AllCheckpointsHook) { static bool Callback(void* acc, int checkpointIdx); };
+HOOK_DEFINE_TRAMPOLINE(CloudSkipHook) { static bool Callback(StageScene* stageScene); };
+HOOK_DEFINE_TRAMPOLINE(SkipBroodalsHook) { static bool Callback(StageScene* stageScene); };
+HOOK_DEFINE_TRAMPOLINE(HintPhotoHook) { static bool Callback(const al::LiveActor* actor, const char* name); };
+HOOK_DEFINE_TRAMPOLINE(ToadRefreshHook) { static bool Callback(const al::LiveActor* actor); };
+HOOK_DEFINE_TRAMPOLINE(NoclipHook) { static void Callback(PlayerActorHakoniwa* player); };
+HOOK_DEFINE_TRAMPOLINE(WorldWarpChangeStageHook) { static void Callback(GameDataHolder* thisPtr, const char* stageName); };
+
+// Cutscene skip hooks
+HOOK_DEFINE_TRAMPOLINE(RsDemoHook) { static bool Callback(const al::Scene* scene); };
+HOOK_DEFINE_TRAMPOLINE(FirstDemoScenarioHook) { static bool Callback(const al::LiveActor* actor); };
+HOOK_DEFINE_TRAMPOLINE(FirstDemoWorldHook) { static bool Callback(const al::Scene* scene); };
+HOOK_DEFINE_TRAMPOLINE(FirstDemoMoonRockHook) { static bool Callback(void* thisPtr); };
+HOOK_DEFINE_TRAMPOLINE(ShowDemoHackHook) { static bool Callback(void* thisPtr); };
+
+static const char* sCustomMoonNames[] = {
+    "Practice Mod", "Hi", "Practice", "Hi BTT", "BTT", "I'm cool", "Super Mario Odyssey", " "
+};
 
 bool IsGotShine::Callback(void* accessor, void* shineInfo)
 {
@@ -33,7 +67,11 @@ bool IsGotShine::Callback(void* accessor, void* shineInfo)
 void SetGotShine::Callback(GameDataHolderWriter writer, void* shineInfo)
 {
     auto* cfg = getConfig();
-    if (!cfg || !cfg->mIsShineRefreshEnabled)
+    if (cfg && cfg->mMoonNameIdx >= 0 && cfg->mMoonNameIdx < 8) {
+        exl::util::RwPages a(exl::util::modules::GetTargetOffset(offsets::ShineRefreshText), 24);
+        strncpy((char*)a.GetRw(), sCustomMoonNames[cfg->mMoonNameIdx], 24);
+    }
+    if (!cfg || !cfg->mIsMoonRefreshEnabled)
         Orig(writer, shineInfo);
 }
 
@@ -72,7 +110,7 @@ static int getMofumofuTarget(int a)
 static bool isEnableCheckpointWarp(MapLayout* thisPtr)
 {
     auto* cfg = getConfig();
-    return (cfg && cfg->mIsEnableWarpsAlways) ? true : thisPtr->isEnableCheckpointWarp();
+    return (cfg && (cfg->mIsAlwaysCheckpoints || cfg->mIsAllCheckpointsEnabled)) ? true : thisPtr->isEnableCheckpointWarp();
 }
 
 static bool isEnableSave(StageScene* scene)
@@ -80,8 +118,6 @@ static bool isEnableSave(StageScene* scene)
     auto* cfg = getConfig();
     return (cfg && cfg->mIsEnableAutosave) ? scene->isEnableSave() : false;
 }
-
-HOOK_DEFINE_TRAMPOLINE(IsPadTriggerA) { static bool Callback(s32 port); };
 
 bool IsPadTriggerA::Callback(s32 port)
 {
@@ -106,13 +142,205 @@ static void setMapTargetUpdateNullNerve(al::IUseNerve* user, const al::Nerve* ne
     al::setNerve(user, nerve);
 }
 
-HOOK_DEFINE_TRAMPOLINE(DoCheckpointTouchNotify) { static void Callback(al::LiveActor * checkpoint); };
-
 void DoCheckpointTouchNotify::Callback(al::LiveActor* checkpoint)
 {
     auto* cfg = getConfig();
     if (!cfg || !cfg->mDisableCheckpointTouching)
         Orig(checkpoint);
+}
+
+void NoDamageHook::Callback(GameDataHolderWriter writer)
+{
+    auto* cfg = getConfig();
+    if (cfg && cfg->mIsNoDamageEnabled) return;
+    Orig(writer);
+}
+
+bool WarpTextHook::Callback(void* accessor)
+{
+    auto* cfg = getConfig();
+    return (cfg && cfg->mIsWarpTextRefreshEnabled) ? false : Orig(accessor);
+}
+
+bool RefreshPurpsHook::Callback(void* accessor, const void* info)
+{
+    auto* cfg = getConfig();
+    return (cfg && cfg->mIsPurpleCoinsRefreshEnabled) ? false : Orig(accessor, info);
+}
+
+void DoorRefreshHook::Callback(al::LiveActor* door, const void* info)
+{
+    Orig(door, info);
+    auto* cfg = getConfig();
+    if (cfg && cfg->mIsDoorRefreshEnabled) {
+        static void (*sSwitchCloseAgain)(al::LiveActor*) = nullptr;
+        if (!sSwitchCloseAgain) {
+            nn::ro::LookupSymbol(reinterpret_cast<uintptr_t*>(&sSwitchCloseAgain), "_ZN14DoorAreaChange16switchCloseAgainEv");
+        }
+        if (sSwitchCloseAgain) sSwitchCloseAgain(door);
+    }
+}
+
+int SeedGrowTimeHook::Callback(const al::LiveActor* actor, const void* id)
+{
+    auto* cfg = getConfig();
+    if (cfg && cfg->mIsSeedsGrowing) return 999999;
+    if (cfg && cfg->mIsSeedsRefreshEnabled) return 0;
+    return Orig(actor, id);
+}
+
+bool SeedUsedHook::Callback(const al::LiveActor* actor, const void* id)
+{
+    auto* cfg = getConfig();
+    if (cfg && cfg->mIsSeedsRefreshEnabled) return false;
+    return Orig(actor, id);
+}
+
+bool KingdomEnterHook::Callback(void* data, int i)
+{
+    auto* cfg = getConfig();
+    return (cfg && cfg->mIsKingdomEnterCutsceneRefreshEnabled) ? false : Orig(data, i);
+}
+
+int DisableMoonLockHook::Callback(GameDataHolder* thisPtr, bool* isCrashList, int worldID)
+{
+    int lockSize = Orig(thisPtr, isCrashList, worldID);
+    auto* cfg = getConfig();
+    return (cfg && cfg->mIsDisableMoonLock) ? 0 : lockSize;
+}
+
+bool AllCheckpointsHook::Callback(void* acc, int checkpointIdx)
+{
+    auto* cfg = getConfig();
+    if (cfg && cfg->mIsAllCheckpointsEnabled) return true;
+    return Orig(acc, checkpointIdx);
+}
+
+bool CloudSkipHook::Callback(StageScene* stageScene)
+{
+    static int functionCalls = 0;
+    auto* cfg = getConfig();
+    if (!cfg || !cfg->mIsSkipCloudEnabled) {
+        functionCalls = 0;
+        return Orig(stageScene);
+    }
+    functionCalls++;
+    if (functionCalls == 2) {
+        functionCalls = 0;
+        if (ILTracker::instance()) {
+            ILTracker::instance()->endRun();
+        }
+        return true;
+    }
+    return false;
+}
+
+bool SkipBroodalsHook::Callback(StageScene* stageScene)
+{
+    auto* cfg = getConfig();
+    if (cfg && cfg->mIsSkipBroodalsEnabled) return true;
+    return Orig(stageScene);
+}
+
+bool HintPhotoHook::Callback(const al::LiveActor* actor, const char* name)
+{
+    auto* cfg = getConfig();
+    if (cfg && (cfg->mIsHintPhotoSpawnEnabled || cfg->mIsLuigiPixelsEnabled)) return true;
+    return Orig(actor, name);
+}
+
+bool ToadRefreshHook::Callback(const al::LiveActor* actor)
+{
+    auto* cfg = getConfig();
+    return (cfg && cfg->mIsCutsceneRefreshEnabled) ? false : Orig(actor);
+}
+
+bool RsDemoHook::Callback(const al::Scene* scene)
+{
+    auto* cfg = getConfig();
+    return (cfg && cfg->mIsAlwaysManualCutscene) ? true : Orig(scene);
+}
+
+bool FirstDemoScenarioHook::Callback(const al::LiveActor* actor)
+{
+    auto* cfg = getConfig();
+    return (cfg && cfg->mIsAlwaysManualCutscene) ? true : Orig(actor);
+}
+
+bool FirstDemoWorldHook::Callback(const al::Scene* scene)
+{
+    auto* cfg = getConfig();
+    return (cfg && cfg->mIsAlwaysManualCutscene) ? true : Orig(scene);
+}
+
+bool FirstDemoMoonRockHook::Callback(void* thisPtr)
+{
+    auto* cfg = getConfig();
+    return (cfg && cfg->mIsAlwaysManualCutscene) ? true : Orig(thisPtr);
+}
+
+bool ShowDemoHackHook::Callback(void* thisPtr)
+{
+    auto* cfg = getConfig();
+    return (cfg && cfg->mIsAlwaysManualCutscene) ? true : Orig(thisPtr);
+}
+
+void WorldWarpChangeStageHook::Callback(GameDataHolder* thisPtr, const char* stageName)
+{
+    if (ILTracker::instance()) {
+        if (ILTracker::instance()->getCurrentKingdom() == KingdomId::Cap) {
+            ILTracker::instance()->notifyCapEnd();
+        } else {
+            ILTracker::instance()->notifyOdyssey();
+        }
+        ILTracker::instance()->notifyBlackScreen();
+    }
+    Orig(thisPtr, stageName);
+}
+
+void NoclipHook::Callback(PlayerActorHakoniwa* player)
+{
+    static bool wasNoclipOn = false;
+    bool isNoclip = (getConfig() && getConfig()->mIsNoclipEnabled);
+    if (!isNoclip && wasNoclipOn)
+        player->endDemoPuppetable();
+    wasNoclipOn = isNoclip;
+
+    if (!isNoclip) {
+        Orig(player);
+        return;
+    }
+
+    player->startDemoPuppetable();
+
+    sead::Vector3f* playerPos = al::getTransPtr(player);
+    sead::Vector3f cameraPos = al::getCameraPos(player, 0);
+
+    sead::Vector2f leftStick = { 0.f, 0.f };
+    al::getJoypadLeftStick(&leftStick, -1);
+
+    float d = sqrtf(
+        (playerPos->x - cameraPos.x) * (playerPos->x - cameraPos.x) +
+        ((playerPos->z - cameraPos.z) * (playerPos->z - cameraPos.z))
+    );
+    if (d > 0.001f) {
+        float speed = 25.0f;
+        if (al::isPadHoldX(-1) || al::isPadHoldY(-1)) speed = 75.0f;
+
+        float vx = (speed / d) * (playerPos->x - cameraPos.x);
+        float vz = (speed / d) * (playerPos->z - cameraPos.z);
+
+        playerPos->x -= leftStick.x * vz;
+        playerPos->z += leftStick.x * vx;
+
+        playerPos->x += leftStick.y * vx;
+        playerPos->z += leftStick.y * vz;
+
+        if (al::isPadHoldZL(-1)) playerPos->y -= speed;
+        if (al::isPadHoldZR(-1)) playerPos->y += speed;
+    }
+
+    Orig(player);
 }
 
 void installPracticeHacks()
@@ -139,6 +367,28 @@ void installPracticeHacks()
     exl::util::RwPages a(exl::util::modules::GetTargetOffset(offsets::ShineRefreshText), 24);
     strncpy((char*)a.GetRw(), "Practice Mod", 24);
 #endif
+
+    NoDamageHook.InstallAtSymbol("_ZN16GameDataFunction12damagePlayerE20GameDataHolderWriter");
+    WarpTextHook.InstallAtSymbol("_ZN16GameDataFunction34isAlreadyShowExplainCheckpointFlagE22GameDataHolderAccessor");
+    RefreshPurpsHook.InstallAtSymbol("_ZN16GameDataFunction16isGotCoinCollectE22GameDataHolderAccessorRKN2al13ActorInitInfoE");
+    DoorRefreshHook.InstallAtSymbol("_ZN14DoorAreaChange4initERKN2al13ActorInitInfoE");
+    SeedGrowTimeHook.InstallAtSymbol("_ZN2rs17getGrowFlowerTimeEPKN2al9LiveActorEPKNS0_11PlacementIdE");
+    SeedUsedHook.InstallAtSymbol("_ZN2rs20isUsedGrowFlowerSeedEPKN2al9LiveActorEPKNS0_11PlacementIdE");
+    KingdomEnterHook.InstallAtSymbol("_ZNK16GameProgressData16isAlreadyGoWorldEi");
+    DisableMoonLockHook.InstallAtSymbol("_ZNK14GameDataHolder18findUnlockShineNumEPbi");
+    AllCheckpointsHook.InstallAtSymbol("_ZN16GameDataFunction22isGotCheckpointInWorldE22GameDataHolderAccessori");
+    CloudSkipHook.InstallAtSymbol("_ZNK10StageScene16isDefeatKoopaLv1Ev");
+    SkipBroodalsHook.InstallAtSymbol("_ZNK10StageScene14isDefeatBossLv1Ev");
+    HintPhotoHook.InstallAtSymbol("_ZN2rs19checkSavedHintPhotoEPKN2al9LiveActorEPKc");
+    ToadRefreshHook.InstallAtSymbol("_ZN2rs34isOnFlagKinopioBrigadeNpcFirstTalkEPKN2al9LiveActorE");
+    WorldWarpChangeStageHook.InstallAtSymbol("_ZN14GameDataHolder31changeNextStageWithDemoWorldWarpEPKc");
+    NoclipHook.InstallAtSymbol("_ZN19PlayerActorHakoniwa8movementEv");
+
+    RsDemoHook.InstallAtSymbol("_ZN2rs11isFirstDemoEPKN2al5SceneE");
+    FirstDemoScenarioHook.InstallAtSymbol("_ZN2rs30isFirstDemoScenarioStartCameraEPKN2al9LiveActorE");
+    FirstDemoWorldHook.InstallAtSymbol("_ZN2rs27isFirstDemoWorldIntroCameraEPKN2al5SceneE");
+    FirstDemoMoonRockHook.InstallAtSymbol("_ZNK12MoonRockData38isEnableShowDemoAfterOpenMoonRockFirstEv");
+    ShowDemoHackHook.InstallAtSymbol("_ZNK18DemoStateHackFirst20isEnableShowHackDemoEv");
 }
 
 } // namespace pe

8162ab85cc268613fa1f42f436f205bc7384d71f
ademjab33-create <adem.jab33@gmail.com>
2026-09-25 23:47:07 +0200

    sync: include/pe/Ghost/GhostPuppetActor.h

--- include/pe/Ghost/GhostPuppetActor.h
+++ include/pe/Ghost/GhostPuppetActor.h
@@ -1,16 +1,44 @@
 #pragma once
+
 #include "al/Library/LiveActor/LiveActor.h"
-namespace al { class ActorInitInfo; }
+#include "pe/Ghost/GhostTypes.h"
+#include <sead/heap/seadDisposer.h>
+
+namespace al {
+struct ActorInitInfo;
+}
 
 namespace pe {
-class GhostPuppetActor : public al::LiveActor {
+
+class GhostPuppetActor : public al::LiveActor, public sead::IDisposer {
 public:
     GhostPuppetActor(const char* name);
-    void init(const al::ActorInitInfo& info) override;
+    ~GhostPuppetActor() override;
+
+    void init(const al::ActorInitInfo& initInfo) override;
+    void initAfterPlacement() override;
     void control() override;
-    void applyPosition(const sead::Vector3f& pos);
+    void movement() override;
+    void makeActorAlive() override;
+    void makeActorDead() override;
+
     void setGhostAlpha(float alpha);
+    float getGhostAlpha() const { return mGhostAlpha; }
+
+    void applyReplayFrame(const ReplayFrame& frame);
+    void startMarioAction(const char* actionName);
+    void startCapAction(const char* actionName);
+
+    al::LiveActor* getCapActor() const { return mCapActor; }
+
 private:
+    void applyShaderTransparency();
+
+    al::LiveActor* mCapActor = nullptr;
     float mGhostAlpha = 0.45f;
+    char mCurrentMarioAnim[64] = {0};
+    char mCurrentCapAnim[64] = {0};
+    bool mIsCapVisible = true;
 };
+
 } // namespace pe

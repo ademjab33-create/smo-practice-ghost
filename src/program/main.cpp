4f23794505df956e6d810b9693499eca7cd026f8
ademjab33-create <adem.jab33@gmail.com>
2026-09-25 15:08:09 +0200

    feat: main.cpp with Menu + Ghost for 1.0.0

--- src/program/main.cpp
+++ src/program/main.cpp
@@ -1,80 +1,98 @@
 #include "al/Library/Memory/MemorySystem.h"
 #include "al/Library/Sequence/Sequence.h"
-#include "al/Library/Controller/JoyPadUtil.h"
 #include "diag/assert.hpp"
 #include "heap/seadExpHeap.h"
 #include "heap/seadHeapMgr.h"
 #include "hook/trampoline.hpp"
 #include "lib.hpp"
 #include "nn/fs.h"
+#include "pe/Hacks/MenuDPadDisable.h"
+#include "pe/Hacks/PracticeHacks.h"
+#include "pe/Menu/Menu.h"
+#include "pe/Menu/UserConfig.h"
 #include "pe/Util/Offsets.h"
 #include "program/imgui_nvn.h"
 #include "pe/Ghost/GhostManager.h"
 #include "pe/Ghost/ILTracker.h"
 #include "pe/Ghost/PBStorage.h"
-#include "pe/Ghost/PBOverlay.h"
 #include <sead/filedevice/seadFileDeviceMgr.h>
 
-static sead::ExpHeap* sGhostHeap = nullptr;
-
 HOOK_DEFINE_TRAMPOLINE(FileDeviceMgrCtor) { static void Callback(sead::FileDeviceMgr * thisPtr); };
-void FileDeviceMgrCtor::Callback(sead::FileDeviceMgr* thisPtr) { Orig(thisPtr); thisPtr->mMountedSd = nn::fs::MountSdCardForDebug("sd").IsSuccess(); }
+void FileDeviceMgrCtor::Callback(sead::FileDeviceMgr* thisPtr)
+{
+    Orig(thisPtr);
+    thisPtr->mMountedSd = nn::fs::MountSdCardForDebug("sd").IsSuccess();
+}
 
 class HakoniwaSequence;
-
 HOOK_DEFINE_TRAMPOLINE(HakoniwaSequenceInit) { static void Callback(HakoniwaSequence * thisPtr, const al::SequenceInitInfo& info); };
-void HakoniwaSequenceInit::Callback(HakoniwaSequence* thisPtr, const al::SequenceInitInfo& info) {
+void HakoniwaSequenceInit::Callback(HakoniwaSequence* thisPtr, const al::SequenceInitInfo& info)
+{
     Orig(thisPtr, info);
-    sGhostHeap = sead::ExpHeap::create(1024 * 1024 * 4, "GhostHeap", al::getSequenceHeap(), 8, sead::ExpHeap::cHeapDirection_Forward, false);
-    sead::ScopedCurrentHeapSetter setter(sGhostHeap);
+
+    pe::getMenuHeap() = sead::ExpHeap::create(1024 * 1024 * 8, "MenuHeap", al::getSequenceHeap(), 8, sead::ExpHeap::cHeapDirection_Forward, false);
+
+    sead::ScopedCurrentHeapSetter setter(pe::getMenuHeap());
     pe::PBStorage::createInstance(nullptr);
     pe::PBStorage::instance()->init();
     pe::ILTracker::createInstance(nullptr);
     pe::ILTracker::instance()->init();
     pe::GhostManager::createInstance(nullptr);
+    pe::Menu::createInstance(nullptr);
 }
 
 HOOK_DEFINE_TRAMPOLINE(HakoniwaSequenceUpdate) { static void Callback(HakoniwaSequence * thisPtr); };
-void HakoniwaSequenceUpdate::Callback(HakoniwaSequence* thisPtr) {
+void HakoniwaSequenceUpdate::Callback(HakoniwaSequence* thisPtr)
+{
     Orig(thisPtr);
-    if (!sGhostHeap) return;
-    sead::ScopedCurrentHeapSetter setter(sGhostHeap);
-    al::Scene* scene = reinterpret_cast<al::Sequence*>(thisPtr)->getCurrentScene();
-    auto* ghostMgr = pe::GhostManager::instance();
-    if (ghostMgr) {
-        ghostMgr->update(scene);
-        if (al::isPadHoldR(-1)) {
-            if (al::isPadTriggerUp(-1))   ghostMgr->startRun();
-            if (al::isPadTriggerDown(-1)) ghostMgr->stopRun();
-            if (al::isPadTriggerLeft(-1)) ghostMgr->resetRun();
-        }
+
+    if (!pe::getMenuHeap()) return;
+    sead::ScopedCurrentHeapSetter setter(pe::getMenuHeap());
+    auto* menu = pe::Menu::instance();
+    if (menu) {
+        al::Scene* scene = reinterpret_cast<al::Sequence*>(thisPtr)->getCurrentScene();
+        menu->update(scene);
     }
 }
 
 HOOK_DEFINE_TRAMPOLINE(SceneEndInitHook) {
     static void Callback(al::Scene* scene, const al::ActorInitInfo& info) {
         Orig(scene, info);
-        if (!sGhostHeap) return;
-        sead::ScopedCurrentHeapSetter setter(sGhostHeap);
+        if (!pe::getMenuHeap()) return;
+        sead::ScopedCurrentHeapSetter setter(pe::getMenuHeap());
         auto* ghostMgr = pe::GhostManager::instance();
-        if (ghostMgr) ghostMgr->initPuppet(info);
+        if (ghostMgr) {
+            ghostMgr->init(info);
+        }
     }
 };
 
-static void drawDbgGui() {
-    if (!sGhostHeap) return;
-    sead::ScopedCurrentHeapSetter setter(sGhostHeap);
-    pe::PBOverlay::draw();
+static void drawDbgGui()
+{
+    if (!pe::getMenuHeap()) return;
+    sead::ScopedCurrentHeapSetter setter(pe::getMenuHeap());
+    auto* menu = pe::Menu::instance();
+    if (menu)
+        menu->draw();
 }
 
-extern "C" void exl_main(void* x0, void* x1) {
+extern "C" void exl_main(void* x0, void* x1)
+{
     exl::hook::Initialize();
+
     FileDeviceMgrCtor::InstallAtOffset(pe::offsets::FileDeviceMgrCtorHookLocation);
     HakoniwaSequenceInit::InstallAtOffset(pe::offsets::HakoniwaSequenceInitHookLocation);
     HakoniwaSequenceUpdate::InstallAtOffset(pe::offsets::HakoniwaSequenceUpdate);
     SceneEndInitHook::InstallAtOffset(pe::offsets::SceneEndInitHookLocation);
+
+    pe::initMenuDPadDisableHooks();
+    pe::installPracticeHacks();
+
     nvnImGui::InstallHooks();
     nvnImGui::addDrawFunc(drawDbgGui);
 }
 
-extern "C" NORETURN void exl_exception_entry() { EXL_ABORT(0x420); }
+extern "C" NORETURN void exl_exception_entry()
+{
+    EXL_ABORT(0x420);
+}

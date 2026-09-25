f034ac41d2adc511873501ebf22086488ca48907
ademjab33-create <adem.jab33@gmail.com>
2026-09-25 18:31:28 +0200

    feat: main.cpp with InstallAtSymbol for 1.0.0

--- src/program/main.cpp
+++ src/program/main.cpp
@@ -84,10 +84,17 @@ extern "C" void exl_main(void* x0, void* x1)
         pe::getConfig() = new pe::UserConfig();
     }
 
+#if GAME_VERSION == 100
+    FileDeviceMgrCtor::InstallAtSymbol("_ZN4sead13FileDeviceMgrC2Ev");
+    HakoniwaSequenceInit::InstallAtSymbol("_ZN16HakoniwaSequence4initERKN2al16SequenceInitInfoE");
+    HakoniwaSequenceUpdate::InstallAtSymbol("_ZN16HakoniwaSequence6updateEv");
+    SceneEndInitHook::InstallAtSymbol("_ZN2al5Scene7endInitERKNS_13ActorInitInfoE");
+#else
     FileDeviceMgrCtor::InstallAtOffset(pe::offsets::FileDeviceMgrCtorHookLocation);
     HakoniwaSequenceInit::InstallAtOffset(pe::offsets::HakoniwaSequenceInitHookLocation);
     HakoniwaSequenceUpdate::InstallAtOffset(pe::offsets::HakoniwaSequenceUpdate);
     SceneEndInitHook::InstallAtOffset(pe::offsets::SceneEndInitHookLocation);
+#endif
 
     pe::initMenuDPadDisableHooks();
     pe::installPracticeHacks();

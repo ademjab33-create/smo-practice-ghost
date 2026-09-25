76fddcf753060be206dabda435c433c71f360572
ademjab33-create <adem.jab33@gmail.com>
2026-09-25 23:47:11 +0200

    sync: include/pe/Ghost/ILTracker.h

--- include/pe/Ghost/ILTracker.h
+++ include/pe/Ghost/ILTracker.h
@@ -1,8 +1,12 @@
 #pragma once
-#include "pe/Ghost/GhostTypes.h"
-#include <heap/seadDisposer.h>
 
-namespace al { class Scene; }
+#include "basis/seadTypes.h"
+#include "pe/Ghost/GhostTypes.h"
+#include <sead/heap/seadDisposer.h>
+
+namespace al {
+class Scene;
+}
 
 namespace pe {
 
@@ -14,35 +18,45 @@ class ILTracker {
 public:
     void init();
     void update(al::Scene* scene);
-    void startRun();
-    void endRun();
-    void resetRun();
-    s64 getElapsedTicks() const;
 
+    // Détection du Royaume et du Segment
+    KingdomId getCurrentKingdom() const { return mCurrentKingdom; }
+    SegmentId getCurrentSegment() const { return mCurrentSegment; }
+    const char* getCurrentKingdomName() const;
+    const char* getCurrentSegmentName() const;
+
+    // Événements déclencheurs (Triggers)
+    void onStageEntry();
     void notifyMoonStory();
     void notifyBlackScreen();
     void notifyOdyssey();
     void notifyMoonEnd();
     void notifyCapEnd();
 
-    KingdomId getCurrentKingdom() const { return mCurrentKingdom; }
-    SegmentId getCurrentSegment() const { return mCurrentSegment; }
-    const char* getCurrentKingdomName() const;
-    const char* getCurrentSegmentName() const;
+    // Contrôle manuel / Timer
+    void startRun();
+    void endRun();
+    void resetRun();
+
+    // Statut
     bool isRunActive() const { return mIsRunActive; }
+    s64 getElapsedTicks() const;
     u32 getElapsedFrames() const { return mElapsedFrames; }
 
 private:
-    void detectKingdom(const char* stageName);
+    void detectKingdomAndSegment(al::Scene* scene);
     void checkSplitConditions();
 
     KingdomId mCurrentKingdom = KingdomId::Unknown;
     SegmentId mCurrentSegment = SegmentId::FullKingdom;
-    char mCurrentStageName[128] = {};
+    char mCurrentStageName[64] = {0};
+
     bool mIsRunActive = false;
     s64 mStartTick = 0;
     s64 mEndTick = 0;
     u32 mElapsedFrames = 0;
+
+    // Compteurs de composants pour les règles de split
     int mMoonStoryCount = 0;
     int mBlackScreenCount = 0;
     int mOdysseyCount = 0;

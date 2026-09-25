96a839ed71eeb5741bdb73efc2c4cb21b8a8e333
ademjab33-create <adem.jab33@gmail.com>
2026-09-25 23:35:48 +0200

    feat: add Practice 1.0 options and Auto Kingdom Timer (include/pe/Menu/Menu.h)

--- include/pe/Menu/Menu.h
+++ include/pe/Menu/Menu.h
@@ -1,86 +1,66 @@
 #pragma once
 
 #include "al/Library/LiveActor/LiveActor.h"
-#include "al/Library/Thread/FunctorV0M.h"
-#include "al/Project/Scene/SceneCreator.h"
+#include "al/Library/Scene/Scene.h"
 #include "container/seadPtrArray.h"
+#include "heap/seadHeap.h"
 #include "imgui.h"
-#include "math/seadQuat.h"
 #include "pe/Menu/Action.h"
 #include "pe/Menu/IComponent.h"
-#include <sead/container/seadPtrArray.h>
+#include "pe/Menu/MenuComponent.h"
+#include "pe/Menu/UserConfig.h"
 #include <sead/heap/seadDisposer.h>
-#include <sead/heap/seadHeap.h>
-#include <sead/math/seadVector.h>
 
 namespace pe {
 
 sead::Heap*& getMenuHeap();
 
-class MenuComponent;
+class Menu : public sead::IDisposer, public IComponent {
+    SEAD_SINGLETON_DISPOSER(Menu)
 
-struct MenuCategory {
-    const char* name;
-    sead::PtrArray<MenuComponent> components;
-};
+public:
+    static constexpr int sCategorySpacing = 6;
+    static constexpr int sCategoryFontSize = 40;
 
-class Menu {
-    bool mIsEnabled = true;
-    sead::PtrArray<IComponent> mComponents;
-    MenuCategory mCategories[6];
+    Menu();
+    void update(al::Scene* scene);
+    void updateInput();
+    void draw() override;
+    void drawExpandedCategory();
+    void drawInfoWindows();
+    bool isEnabled() const { return mIsEnabled; }
+
+    void savePosition(al::LiveActor* player);
+    void loadPosition(al::LiveActor* player);
+    void setLatestMapTarget(void* target) { mLastMapTarget = target; }
+    void callAction(ActionType type);
+
+    struct Category {
+        const char* name;
+        sead::PtrArray<MenuComponent> components;
+    };
+
+    Category& getCurrentCategory() { return mCategories[mCurrentCategory]; }
+    int getCategoryHeight(int id);
+
+private:
+    bool mIsEnabled = false;
     int mCurrentCategory = 0;
-    bool mIsExpandedCurrentCategory = false;
     int mCurrentComponentInCategory = 0;
+    bool mIsExpandedCurrentCategory = false;
     bool mIsFocusedOnCurrentComponent = false;
-    ImVec2 mBgSize;
-    al::Scene* mScene = nullptr;
 
-    sead::Vector3f mSavedPosition;
-    sead::Quatf mSavedQuat;
+    Category mCategories[9];
+    sead::PtrArray<IComponent> mComponents;
+
+    ImVec2 mBgSize = ImVec2(0, 0);
+    sead::Vector3f mSavedPosition = sead::Vector3f(0, 0, 0);
+    sead::Quatf mSavedQuat = sead::Quatf(0, 0, 0, 0);
     bool mIsSavedPos = false;
     void* mLastMapTarget = nullptr;
 
+    al::Scene* mScene = nullptr;
     int mTimer = 0;
-
-    int getCategoryHeight(int idx) const { return (sCategorySpacing * 2 + sCategoryFontSize) * idx; }
-    void updateInput();
-    void drawExpandedCategory();
-    MenuCategory& getCurrentCategory() { return mCategories[mCurrentCategory]; }
-
-    SEAD_SINGLETON_DISPOSER(Menu)
-public:
-    Menu();
-
-    void update(al::Scene* scene);
-    void draw();
-    bool isEnabled() const { return mIsEnabled; }
-    void callAction(ActionType type);
-
-    class CallActionFunctor : public al::FunctorBase {
-    private:
-        Menu* mPtr = nullptr;
-        ActionType mType = ActionType::None;
-
-    public:
-        CallActionFunctor(Menu* ptr, ActionType type)
-            : mPtr(ptr)
-            , mType(type)
-        {
-        }
-
-        void operator()() const override { mPtr->callAction(mType); };
-        FunctorBase* clone() const override { return new CallActionFunctor(*this); };
-
-        ~CallActionFunctor() override = default;
-    };
-
-    __attribute__((noinline)) void savePosition(al::LiveActor* playerBase);
-    __attribute__((noinline)) void loadPosition(al::LiveActor* playerBase);
-
-    static constexpr int sCategoryFontSize = 25;
-    static constexpr int sCategorySpacing = 4;
-
-    void setLatestMapTarget(void* target) { mLastMapTarget = target; }
 };
 
 } // namespace pe

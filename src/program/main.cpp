d6a71b94d13b8e563ecb482a1249fad7af2f9b2b
ademjab33-create <adem.jab33@gmail.com>
2026-09-26 00:54:30 +0200

    sync: src/program/main.cpp

--- src/program/main.cpp
+++ src/program/main.cpp
@@ -22,36 +22,37 @@ void FileDeviceMgrCtor::Callback(sead::FileDeviceMgr* thisPtr)
 }
 
 class HakoniwaSequence;
+static HakoniwaSequence* sHakoniwaSequence = nullptr;
+
 HOOK_DEFINE_TRAMPOLINE(HakoniwaSequenceInit) { static void Callback(HakoniwaSequence * thisPtr, const al::SequenceInitInfo& info); };
 void HakoniwaSequenceInit::Callback(HakoniwaSequence* thisPtr, const al::SequenceInitInfo& info)
 {
     Orig(thisPtr, info);
 
-    pe::getMenuHeap() = sead::ExpHeap::create(1024 * 1024 * 8, "MenuHeap", al::getSequenceHeap(), 8, sead::ExpHeap::cHeapDirection_Forward, false);
+    sHakoniwaSequence = thisPtr;
+
+    auto* seqHeap = al::getSequenceHeap();
+    pe::getMenuHeap() = sead::ExpHeap::create(1024 * 512, "MenuHeap", seqHeap, 8, sead::ExpHeap::cHeapDirection_Forward, false);
+    if (!pe::getMenuHeap()) {
+        pe::getMenuHeap() = seqHeap;
+    }
 
     sead::ScopedCurrentHeapSetter setter(pe::getMenuHeap());
     pe::Menu::createInstance(nullptr);
-}
-
-HOOK_DEFINE_TRAMPOLINE(HakoniwaSequenceUpdate) { static void Callback(HakoniwaSequence * thisPtr); };
-void HakoniwaSequenceUpdate::Callback(HakoniwaSequence* thisPtr)
-{
-    Orig(thisPtr);
-
-    sead::ScopedCurrentHeapSetter setter(pe::getMenuHeap());
-    auto* menu = pe::Menu::instance();
-    if (menu) {
-        al::Scene* scene = *reinterpret_cast<al::Scene**>(reinterpret_cast<u8*>(thisPtr) + 0xb0);
-        menu->update(scene);
-    }
 }
 
 static void drawDbgGui()
 {
     sead::ScopedCurrentHeapSetter setter(pe::getMenuHeap());
     auto* menu = pe::Menu::instance();
-    if (menu)
+    if (menu) {
+        al::Scene* scene = nullptr;
+        if (sHakoniwaSequence) {
+            scene = *reinterpret_cast<al::Scene**>(reinterpret_cast<u8*>(sHakoniwaSequence) + 0xb0);
+        }
+        menu->update(scene);
         menu->draw();
+    }
 }
 
 extern "C" void exl_main(void* x0, void* x1)
@@ -60,7 +61,6 @@ extern "C" void exl_main(void* x0, void* x1)
 
     FileDeviceMgrCtor::InstallAtOffset(pe::offsets::FileDeviceMgrCtorHookLocation);
     HakoniwaSequenceInit::InstallAtOffset(pe::offsets::HakoniwaSequenceInitHookLocation);
-    HakoniwaSequenceUpdate::InstallAtOffset(pe::offsets::HakoniwaSequenceUpdate);
 
     pe::initMenuDPadDisableHooks();
     pe::installPracticeHacks();

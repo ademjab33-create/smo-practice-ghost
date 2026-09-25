6856c3ef75dcab49d5f0a910e191f2c57d06d142
ademjab33-create <adem.jab33@gmail.com>
2026-09-25 20:32:21 +0200

    fix: guard 1.3.0 hid hooks in imgui_nvn.cpp

--- src/program/imgui_nvn.cpp
+++ src/program/imgui_nvn.cpp
@@ -205,11 +205,13 @@ void nvnImGui::procDraw()
 void nvnImGui::InstallHooks()
 {
     NvnBootstrapHook::InstallAtSymbol("nvnBootstrapLoader");
+#if GAME_VERSION == 130
     DisableFullKeyState::InstallAtSymbol("_ZN2nn3hid6detail13GetNpadStatesEPiPNS0_16NpadFullKeyStateEiRKj");
     DisableHandheldState::InstallAtSymbol("_ZN2nn3hid6detail13GetNpadStatesEPiPNS0_17NpadHandheldStateEiRKj");
     DisableJoyDualState::InstallAtSymbol("_ZN2nn3hid6detail13GetNpadStatesEPiPNS0_16NpadJoyDualStateEiRKj");
     DisableJoyLeftState::InstallAtSymbol("_ZN2nn3hid6detail13GetNpadStatesEPiPNS0_16NpadJoyLeftStateEiRKj");
     DisableJoyRightState::InstallAtSymbol("_ZN2nn3hid6detail13GetNpadStatesEPiPNS0_17NpadJoyRightStateEiRKj");
+#endif
 }
 
 bool nvnImGui::InitImGui()
